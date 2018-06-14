194af1c7c13df34eec08534d22370c02c51b69f8
SiezeTheGoat <38844949+SiezeTheGoat@users.noreply.github.com>
2018-06-14 09:45:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Sieze') {
+    	message.reply('THE SAVAGE!');
+  	}
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
